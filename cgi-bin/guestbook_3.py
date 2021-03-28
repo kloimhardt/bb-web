54e595037f6de652d00b7912c88c7cc61ecb299e
kloimhardt <markus.agwin@gmx.net>
2021-03-28 16:37:52 +0200

    add the egg files

--- cgi-bin/guestbook_3.py
+++ cgi-bin/guestbook_3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+sys.path.append("cgi-bin/hy-0.20.0+129.g9733a3dd-py3.6.egg")
+sys.path.append("cgi-bin/transit_python-0.8.284-py3.6.egg")
 import os
 from transit.writer import Writer
 from transit.reader import Reader
